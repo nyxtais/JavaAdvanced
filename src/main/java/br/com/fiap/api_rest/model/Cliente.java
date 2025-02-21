a14021e18ceae155721f2bf05e59086fff0d2431
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-21 12:04:09 -0300

    aula21/02

--- src/main/java/br/com/fiap/api_rest/model/Cliente.java
+++ src/main/java/br/com/fiap/api_rest/model/Cliente.java
@@ -4,45 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Entity
+@Data
+@AllArgsConstructor
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private int idade;
-
-    public Cliente() {
-    }
-
-    public Cliente(Long id, String nome, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
+    private String email;
+    private String senha;
+    private String cpf;
+    private Categoria categoria;
 }
--- src/main/java/br/com/fiap/api_rest/service/ClienteService.java
+++ src/main/java/br/com/fiap/api_rest/service/ClienteService.java
@@ -3,6 +3,7 @@ package br.com.fiap.api_rest.service;
 import br.com.fiap.api_rest.dto.ClienteRequest;
 import br.com.fiap.api_rest.dto.ClienteResponse;
 import br.com.fiap.api_rest.model.Cliente;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,7 +12,14 @@ import java.util.List;
 @Service
 public class ClienteService {
     public Cliente requestToCliente(ClienteRequest clienteRequest) {
-        return new Cliente(null, clienteRequest.getNome(), clienteRequest.getIdade());
+        return new Cliente(null,
+                clienteRequest.getNome(),
+                clienteRequest.getIdade(),
+                clienteRequest.getEmail(),
+                clienteRequest.getSenha(),
+                clienteRequest.getCpf(),
+                clienteRequest.getCategoria());
+
     }
 
     public ClienteResponse clienteToResponse(Cliente cliente) {
