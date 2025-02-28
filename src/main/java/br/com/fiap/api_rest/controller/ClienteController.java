b79b72a917897779e3db42723dbd3eae8604938f
nyxtais <nyxtais@gmail.com>
2025-02-28 11:53:52 -0300

    28/02

--- src/main/java/br/com/fiap/api_rest/controller/ClienteController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ClienteController.java
@@ -5,7 +5,6 @@ import br.com.fiap.api_rest.dto.ClienteResponse;
 import br.com.fiap.api_rest.model.Cliente;
 import br.com.fiap.api_rest.repository.ClienteRepository;
 import br.com.fiap.api_rest.service.ClienteService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,11 +26,11 @@ public class ClienteController {
     @Autowired
     ClienteService clienteService;
 
-    // Create, Read, Update, Delete - CRUD
-    // Post, Get, Put, Delete - Verbos HTTP correspondentes
+    // Create, Read, Update, Delete
+    // Post, Get, Put, Delete
 
     @PostMapping
-    public ResponseEntity<Cliente> createCliente(@Valid @RequestBody ClienteRequest cliente) {
+    public ResponseEntity<Cliente> createCliente(@RequestBody ClienteRequest cliente) {
         Cliente clienteSalvo = clienteRepository.save(clienteService.requestToCliente(cliente));
         return new ResponseEntity<>(clienteSalvo, HttpStatus.CREATED);
     }
@@ -41,7 +41,7 @@ public class ClienteController {
         return new ResponseEntity<>(clienteService.findAll(pageable), HttpStatus.OK);
     }
 
-    // PathVariable = parâmetro diretamente na URL, ex: /clientes/1
+    // PathVariable = parâmetro na URL, ex: /clientes/1
     // RequestParam = parâmetro como query, ex: /clientes/?id=1
     @GetMapping("/{id}")
     public ResponseEntity<ClienteResponse> readCliente(@PathVariable Long id) {
@@ -49,7 +49,7 @@ public class ClienteController {
         if (cliente.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(clienteService.clienteToResponse(cliente.get()), HttpStatus.OK);
+        return new ResponseEntity<>(clienteService.clienteToResponse(cliente.get(), false), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
--- src/main/java/br/com/fiap/api_rest/service/ClienteService.java
+++ src/main/java/br/com/fiap/api_rest/service/ClienteService.java
@@ -1,5 +1,6 @@
 package br.com.fiap.api_rest.service;
 
+import br.com.fiap.api_rest.controller.ClienteController;
 import br.com.fiap.api_rest.dto.ClienteRequest;
 import br.com.fiap.api_rest.dto.ClienteResponse;
 import br.com.fiap.api_rest.model.Cliente;
@@ -8,10 +9,14 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class ClienteService {
@@ -33,14 +38,28 @@ public class ClienteService {
                 clienteRequest.getCategoria());
     }
 
-    public ClienteResponse clienteToResponse(Cliente cliente) {
-        return new ClienteResponse(cliente.getId(), cliente.getNome());
+    public ClienteResponse clienteToResponse(Cliente cliente, boolean self) {
+        Link link;
+        if (self) {
+            link = linkTo
+                    (methodOn(
+                                    ClienteController.class
+                            ).readCliente(cliente.getId())
+                    ).withSelfRel();
+        } else {
+            link = linkTo(
+                    methodOn(
+                            ClienteController.class
+                    ).readClientes(0)
+            ).withRel("Lista de Clientes");
+        }
+        return new ClienteResponse(cliente.getId(), cliente.getNome(), link);
     }
 
     public List<ClienteResponse> clientesToResponse(List<Cliente> clientes) {
         List<ClienteResponse> clientesResponse = new ArrayList<>();
         for (Cliente cliente : clientes) {
-            clientesResponse.add(clienteToResponse(cliente));
+            clientesResponse.add(clienteToResponse(cliente, true));
         }
         return clientesResponse;
         // return clientes.stream().map(this::clienteToResponse).collect(Collectors.toList());
