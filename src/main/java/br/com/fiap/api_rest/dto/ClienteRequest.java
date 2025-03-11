7b7711290980218179fc03350223baa25ecbc617
nyxtais <nyxtais@gmail.com>
2025-03-11 11:59:25 -0300

    11/03

--- src/main/java/br/com/fiap/api_rest/dto/ClienteRequest.java
+++ src/main/java/br/com/fiap/api_rest/dto/ClienteRequest.java
@@ -2,19 +2,17 @@ package br.com.fiap.api_rest.dto;
 
 import br.com.fiap.api_rest.model.Categoria;
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.hibernate.validator.constraints.br.CPF;
 
-@Data
-@AllArgsConstructor
 public class ClienteRequest {
-    @NotBlank(message = "O no me é obrigatório")
-    @Size(min = 5, max = 150, message = "O nome deve ter ter entre 3 e 150 caracteres")
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 3, max = 150,
+            message = "O nome deve ter entre 3 e 150 caracteres")
     private String nome;
-    @Min(value = 18, message = "O cliente deve ter no mínimo 18 anos")
+    @Min(value = 18,
+            message = "O cliente deve ter no mínimo 18 anos")
     private int idade;
-    @Email(message = "Email fora fo formato correto")
+    @Email(message = "Email fora do formato correto")
     private String email;
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!,#])[A-Za-z\\d\\W]{8,}$",
             message = "A senha deve conter no mínimo 8 caracteres, letras maiúsculas e minúsculas, números e caracteres especiais (@!,#)")
@@ -23,4 +21,64 @@ public class ClienteRequest {
     private String cpf;
     @NotNull(message = "A categoria é obrigatória")
     private Categoria categoria;
+
+    public ClienteRequest() {
+    }
+
+    public ClienteRequest(String nome, int idade, String email, String senha, String cpf, Categoria categoria) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
